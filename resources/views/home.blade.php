78c05c278a35eb807976abb076a5077754436dd6
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-05-11 14:22:40 +0200

    now can add some prodcut to cart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,9 @@
     @dump ($product1)
     @php
         echo $product1 -> getPrice() . ' $';
+        $customer -> getCart() -> addProductToCart($product1);
+        $customer -> getCart() -> addProductToCart($product1);
+        echo $customer -> getAmountCart();
     @endphp
 </main>
 @endsection
